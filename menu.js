3dc06469c15fba2bcc1446220004c72a9a1f3dcd
julian <juliansanvill@hotmail.com>
2023-03-02 17:54:43 -0500

    first commit

---
+++ menu.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("menu-button").addEventListener("click",showMenu)
+    document.getElementById("close-menu").addEventListener("click",closeMenu)
+
+   function showMenu(){
+    const menu = document.querySelector("section.menu");
+    menu.style.height="100%"
+   }
+   function closeMenu(){
+    const menu=document.querySelector("section.menu");
+    menu.style.height="0";
+   }
+})

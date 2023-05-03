9d78fada5e0f7937b1c64a4baca4f78d6758a7ec
julian <juliansanvill@hotmail.com>
2023-05-03 12:06:43 -0500

    add styles in menu and delete icons and style icons

--- menu.js
+++ menu.js
@@ -1,13 +1,14 @@
-document.addEventListener("DOMContentLoaded",function(){
-    document.getElementById("menu-button").addEventListener("click",showMenu)
-    document.getElementById("close-menu").addEventListener("click",closeMenu)
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("menu-button").addEventListener("click", showMenu);
+  document.getElementById("close-menu").addEventListener("click", closeMenu);
 
-   function showMenu(){
+  function showMenu() {
     const menu = document.querySelector("section.menu");
-    menu.style.height="100%"
-   }
-   function closeMenu(){
-    const menu=document.querySelector("section.menu");
-    menu.style.height="0";
-   }
-})
+    menu.style.height = "100%";
+  }
+  function closeMenu() {
+    const menu = document.querySelector("section.menu");
+    menu.style.height = "0";
+    menu.style.overflox = "none";
+  }
+});
